b954a34fecdab94d58d90019a36addc4e9ec82d8
Samuel Castro <samuel.castro@adclick.pt>
2015-11-11 11:37:49 +0000

    quick fix

--- src/ListBroking/AppBundle/Engine/Filter/LockFilter/CampaignLockFilter.php
+++ src/ListBroking/AppBundle/Engine/Filter/LockFilter/CampaignLockFilter.php
@@ -61,10 +61,11 @@ class CampaignLockFilter implements LockFilterInterface
                 $orX->addMultiple(array(
                     // check for locks on the campaign
                     $qb->expr()
-                       ->andX('locks.type = :campaign_locks_type', "locks.campaign = :campaign_locks_campaign_id_{$key}", "(locks.expiration_date >= :campaign_locks_filter_expiration_date_{$key})"),
+                       ->andX('locks.type = :campaign_locks_type', "locks.campaign = :campaign_locks_campaign_id_{$key}", "(locks.lock_date >= :campaign_locks_filter_expiration_date_{$key})", "(locks
+                       .expiration_date >= CURRENT_TIMESTAMP())"),
                     // Check for locks on the parent (client)
                     $qb->expr()
-                       ->andX('locks.type = :campaign_locks_client_type', "locks.client = :campaign_locks_client_id_{$key}", "(locks.expiration_date >= CURRENT_TIMESTAMP())")
+                       ->andX('locks.type = :campaign_locks_client_type', "locks.client = :campaign_locks_client_id_{$key}", "(locks.lock_date >= CURRENT_TIMESTAMP())")
                 ))
                 ;
 
