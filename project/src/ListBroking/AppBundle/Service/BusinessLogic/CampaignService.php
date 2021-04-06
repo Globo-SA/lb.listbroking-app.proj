1869bb89abb4e1718b96eb74ea4701fde9676052
João Cortez <joaocortez@MacBook-Pro.local>
2021-04-06 18:12:09 +0100

    feat: replace GLOBO mailgun account

--- project/src/ListBroking/AppBundle/Service/BusinessLogic/CampaignService.php
+++ project/src/ListBroking/AppBundle/Service/BusinessLogic/CampaignService.php
@@ -3,12 +3,10 @@
 namespace ListBroking\AppBundle\Service\BusinessLogic;
 
 use ListBroking\AppBundle\Entity\Campaign;
-use ListBroking\AppBundle\Entity\Client;
 use ListBroking\AppBundle\Enum\HttpStatusCodeEnum;
 use ListBroking\AppBundle\Repository\CampaignRepositoryInterface;
 use ListBroking\AppBundle\Repository\ClientRepositoryInterface;
 use ListBroking\AppBundle\Service\Base\BaseService;
-use ListBroking\AppBundle\Service\External\HurryService;
 
 /**
  * ListBroking\AppBundle\Service\BusinessLogic\StagingService
@@ -26,25 +24,17 @@ class CampaignService extends BaseService implements CampaignServiceInterface
     protected $clientRepository;
 
     /**
-     * @var HurryService
-     */
-    protected $hurryService;
-
-    /**
      * StagingService constructor.
      *
      * @param CampaignRepositoryInterface $campaignRepository
      * @param ClientRepositoryInterface   $clientRepository
-     * @param HurryService                $hurryService
      */
     public function __construct(
         CampaignRepositoryInterface $campaignRepository,
-        ClientRepositoryInterface $clientRepository,
-        HurryService $hurryService
+        ClientRepositoryInterface $clientRepository
     ) {
         $this->campaignRepository = $campaignRepository;
         $this->clientRepository   = $clientRepository;
-        $this->hurryService       = $hurryService;
     }
 
     /**
@@ -58,29 +48,6 @@ class CampaignService extends BaseService implements CampaignServiceInterface
             throw new \Exception('An invalid client_id was provided', HttpStatusCodeEnum::HTTP_STATUS_CODE_BAD_REQUEST);
         }
 
-        $accountId = $this->getAccountId($client);
-
-        return $this->campaignRepository->createCampaign($client, $accountId, $campaignData);
+        return $this->campaignRepository->createCampaign($client, null, $campaignData);
     }
-
-    /**
-     * @param Client $client
-     *
-     * @return mixed|null
-     */
-    private function getAccountId(Client $client)
-    {
-        $accountId = null;
-        try {
-            foreach ($this->hurryService->fetchAccounts() as $hurryAccount) {
-                if (strtolower(trim($hurryAccount->client_name)) == strtolower(trim($client->getName()))) {
-                    $accountId = $hurryAccount->id;
-                }
-            }
-        }catch (\Exception $exception){
-        }
-
-        return $accountId;
-    }
-
 }
